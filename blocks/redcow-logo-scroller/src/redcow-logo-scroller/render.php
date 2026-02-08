c6052b15432aa44196a47759b1ccb1a648d8dec9
madmachinations <14132673+madmachinations@users.noreply.github.com>
2026-02-08 13:56:59 +0000

    tweaks

--- blocks/redcow-logo-scroller/src/redcow-logo-scroller/render.php
+++ blocks/redcow-logo-scroller/src/redcow-logo-scroller/render.php
@@ -4,7 +4,7 @@
  */
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<div class="logo-scroller live ">
+	<div class="logo-scroller live">
 		<div class="logo-track <?php if($attributes['direction'] == "right-to-left"){echo "left";} else {echo "right";} ?>">
 			<?php echo $content . $content; ?>
 		</div>
